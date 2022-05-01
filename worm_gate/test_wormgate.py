409c7ce59d1ca6464a7caf0292520f2a230a6909
Mike Murphy <michael.j.murphy@uit.no>
2022-05-01 13:11:20 +0200

    gate: Add tests for the HTTP server upload

--- worm_gate/test_wormgate.py
+++ worm_gate/test_wormgate.py
@@ -142,5 +142,72 @@ class TestWormGateCore(unittest.TestCase):
         finally:
             gate.cleanup_all()
 
+class TestHttp(unittest.TestCase):
+
+    GATE_STARTUP_TIME = 0.1
+
+    def do_upload_test(self, curl_cmd, expect_response_code=None, expect_in_log=None):
+        # Start a worm gate process.
+        worm_gate_proc = subprocess.Popen(
+                "python3 ./wormgate.py --port 8000 --loglevel DEBUG".split(),
+                stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        time.sleep(self.GATE_STARTUP_TIME)
+
+        # Use curl to send a request to the gate.
+        curl_proc = None
+        curl_out = None
+        try:
+            curl_proc = subprocess.run(curl_cmd,
+                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            curl_out = curl_proc.stdout.decode()
+        finally:
+            # Terminate the gate process and wait for it to stop.
+            worm_gate_proc.terminate()
+            (gate_log, _) = worm_gate_proc.communicate()
+            gate_log = gate_log.decode()
+
+        # Format curl and log output for display on error
+        output_display = f"\n\nCURL OUTPUT:\n{curl_out}\n\nGATE LOG:\n{gate_log}\n"
+
+        # Test assertions
+
+        self.assertEqual(curl_proc.returncode, 0,
+                msg = "Expected zero (success) return code from curl process" + output_display)
+
+        if expect_response_code:
+            self.assertIn(f"HTTP/1.0 {expect_response_code}", curl_out,
+                    msg = "Expected to see HTTP response code in curl output" + output_display)
+
+        if expect_in_log:
+            self.assertIn(expect_in_log, gate_log,
+                    msg = "Expected to see specific output in server log" + output_display)
+
+    def test_upload_content_length(self):
+        curl_cmd = "curl -v -X POST http://localhost:8000/worm_entrance".split()
+        curl_cmd = curl_cmd + ["--data-binary", hello_worm]
+        # Curl's default is to use the Content-Length header.
+        self.do_upload_test(curl_cmd,
+                expect_response_code=200,
+                expect_in_log="Hello, World!")
+
+    def test_upload_chunked(self):
+        curl_cmd = "curl -v -X POST http://localhost:8000/worm_entrance".split()
+        curl_cmd = curl_cmd + ["--data-binary", hello_worm]
+        # Use Transfer-Encoding instead of Content-Length when sending.
+        curl_cmd = curl_cmd + ["-H", "Transfer-Encoding: chunked"]
+        self.do_upload_test(curl_cmd,
+                expect_response_code=200,
+                expect_in_log="Hello, World!")
+
+    def test_upload_bad(self):
+        curl_cmd = "curl -v -X POST http://localhost:8000/worm_entrance".split()
+        curl_cmd = curl_cmd + ["--data-binary", hello_worm]
+        # Remove the Content-Length header without providing Transfer-Encoding.
+        # The server will not know how to determine the end of the request.
+        curl_cmd = curl_cmd + ["-H", "Content-Length:"]
+        self.do_upload_test(curl_cmd,
+                expect_response_code=400,
+                expect_in_log="Unsupported encoding/length")
+
 if __name__ == "__main__":
     unittest.main()
